ffb6e49cc9f61b5cd7925500b61d377da025094b
elod.sandor <selod00@gmail.com>
2025-09-10 18:48:08 +0200

    Add Icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import Input from './components/Input/Input';
 import TextArea from './components/TextArea/TextArea';
 import Checkbox from './components/Checkbox/Checkbox';
+import Button from './components/Button/Button';
+import IconButton from './components/Button/IconButton';
+import IconDelete from './components/Icons/IconDelete';
+import IconStar from './components/Icons/IconStar';
 
 function App() {
     const [test, setTest] = useState('');
@@ -27,6 +31,18 @@ function App() {
             />
             <br />
             <Checkbox label="Default Facility" description="Default Facility" />
+            <br />
+            <Button label="Click Me" variant="primary" onClick={() => alert('Button Clicked!')} />
+            <br />
+            <Button label="Click Me" variant="secondary" onClick={() => alert('Button Clicked!')} />
+            <br />
+            <IconButton onClick={() => alert('Icon Button Clicked!')}>
+                <IconDelete />
+            </IconButton>
+            <br />
+            <IconButton onClick={() => alert('Icon Button Clicked!')}>
+                <IconStar />
+            </IconButton>
         </div>
     );
 }
