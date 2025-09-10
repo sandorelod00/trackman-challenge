20da84ed535cda22d9f1897ef554c56eff9418cc
elod.sandor <selod00@gmail.com>
2025-09-10 16:20:50 +0200

    set up eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
@@ -19,5 +20,11 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    }
   },
 ])
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    printWidth: 100,
+  }
