ce15ebb84728b1eddf73b17c22602f13f9846b7d
elod.sandor <selod00@gmail.com>
2025-09-11 17:53:36 +0200

    Skeleton for FacilityFormPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import NavBar from './components/navigation/NavBar';
 
 function App() {
     return (
-        <>
+        <div>
             <NavBar />
             <main className="w-full max-w-[1200px] mx-auto px-4">
                 <Outlet />
             </main>
-        </>
+        </div>
     );
 }
 
--- src/pages/FacilityFormPage.tsx
+++ src/pages/FacilityFormPage.tsx
@@ -1,7 +1,13 @@
 import { useParams } from 'react-router-dom';
+import FacilityForm from '../components/facilityForm/FaclilityForm';
 
 const FacilityFormPage = () => {
     const { id } = useParams<{ id: string }>();
-    return <div>Facility Form Page {id}</div>;
+    return (
+        <div className='flex flex-col'>
+            <h1>Create a New Facility</h1>
+            <FacilityForm />
+        </div>
+    )
 };
 export default FacilityFormPage;
