7a29a4387bf3e35e47701ed14ea67c3d85ce71ac
elod.sandor <selod00@gmail.com>
2025-09-12 13:00:42 +0200

    Interactive form with react form hook

--- src/components/pages/FacilityFormPage/FaclilityForm.tsx
+++ src/components/pages/FacilityFormPage/FaclilityForm.tsx
@@ -1,33 +1,100 @@
-import Button from "../../core/Button/Button";
-import Checkbox from "../../core/Checkbox/Checkbox";
-import Input from "../../core/Input/Input";
-import TextArea from "../../core/TextArea/TextArea";
+import { type FC } from "react"
+import { useForm, type SubmitHandler } from "react-hook-form"
+import Button from "../../core/Button/Button"
+import Checkbox from "../../core/Checkbox/Checkbox"
+import Input from "../../core/Input/Input"
+import TextArea from "../../core/TextArea/TextArea"
+import type { Facility } from "../../../types/Facility"
 
+interface FacilityFormProps {
+    initialFacility?: FacilityFormInputs
+}
 
-const FacilityForm = () => {
+type FacilityFormInputs = Omit<Facility, "id" | "isOpen">
+
+const FacilityForm: FC<FacilityFormProps> = ({ initialFacility }) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FacilityFormInputs>({
+        defaultValues: initialFacility ?? {
+            name: "",
+            address: "",
+            description: "",
+            imageUrl: "",
+            defaultFacility: false,
+            openingHours: "",
+            closingHours: "",
+        },
+    })
+
+    const onSubmit: SubmitHandler<FacilityFormInputs> = (data: FacilityFormInputs) => {
+        console.log("Submitted:", data)
+    }
+
     return (
-        <form className="bg-white shadow[#0C0C0D0D] py-4 px-6">
+        <form className="bg-white shadow[#0C0C0D0D] py-4 px-6" onSubmit={handleSubmit(onSubmit)}>
             <h2>Facility Information</h2>
-            <Input type="text" label="Facility Name" name="facilityName" required />
-            <Input type="text" label="Address" name="location" required />
-            <TextArea label="Description" name="description" required />
-            <Input type="text" label="Cover Image URL" name="coverImageUrl" required />
+
+            <Input
+                type="text"
+                label="Facility Name"
+                {...register("name", { required: "Facility name is required" })}
+                error={errors.name?.message}
+            />
+
+            <Input
+                type="text"
+                label="Location"
+                {...register("address", { required: "Location is required" })}
+                error={errors.address?.message}
+            />
+
+            <TextArea
+                label="Description"
+                {...register("description", { required: "Description is required" })}
+                error={errors.description?.message}
+            />
+
+            <Input
+                type="text"
+                label="Cover Image URL"
+                {...register("imageUrl", {
+                    required: "Image URL is required",
+                })}
+                error={errors.imageUrl?.message}
+            />
+
             <Checkbox
                 label="Default Facility"
                 description="Setting this facility as default will override the currently marked default facility."
-                name="isDefault"
+                {...register("defaultFacility")}
             />
+
             <h2>Working Hours</h2>
-            <div className="flex">
-                <Input type="time" label="Opening Time" name="openingTime" required />
-                <Input type="time" label="Closing Time" name="closingTime" required />
+            <div className="flex gap-4">
+                <Input
+                    type="time"
+                    label="Opening Time"
+                    {...register("openingHours", { required: "Opening time is required" })}
+                    error={errors.openingHours?.message}
+                />
+
+                <Input
+                    type="time"
+                    label="Closing Time"
+                    {...register("closingHours", { required: "Closing time is required" })}
+                    error={errors.closingHours?.message}
+                />
             </div>
-            <div className="flex justify-end gap-2">
-                <Button label={'Cancel'} variant="secondary" />
-                <Button label="Create Facility" variant="primary" />
+
+            <div className="flex justify-end gap-2 mt-4">
+                <Button label="Cancel" variant="secondary" type="button" />
+                <Button label="Create Facility" variant="primary" type="submit" />
             </div>
         </form>
-    );
-};
+    )
+}
 
-export default FacilityForm;
+export default FacilityForm
