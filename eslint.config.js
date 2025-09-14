274af183b81da4ade7bdd7518adf9ad14228ddb9
elod.sandor <selod00@gmail.com>
2025-09-14 17:28:18 +0200

    fix eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -15,6 +16,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,7 +26,7 @@ export default tseslint.config([
       prettier,
     },
     rules: {
-      'prettier/prettier': ['warn', {"usePrettierrc": "true"}],
+      'prettier/prettier': ['warn'],
     }
   },
 ])
